1b88c972f77f1571e2c02c89d513f31eb7b9c4f7
Jerryzs <jerrysizhang@gmail.com>
2021-09-30 09:51:06 -0400

    docs(typings): add json types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,4 +2,9 @@ import GlobalObject from './global'
 
 declare global {
   type $0 = typeof GlobalObject
+
+  type JSONPrimitive = string | number | boolean | null
+  type JSONValue = JSONPrimitive | JSONObject | JSONArray
+  type JSONObject = { [member: string]: JSONValue }
+  type JSONArray = Array<JSONValue>
 }
