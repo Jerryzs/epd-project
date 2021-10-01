f20c30a8cf76132b79bb6012b30911f9489bbe09
Jerryzs <jerrysizhang@gmail.com>
2021-09-30 20:49:22 -0400

    chore(App): import bootstrap to global namespace

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from 'next/app'
 
+if (typeof window !== 'undefined') {
+  window.bootstrap = require('bootstrap')
+}
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
 }
