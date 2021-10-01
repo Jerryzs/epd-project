0781763cea5dba556b0ca5622d62b4d3b2868f8b
Jerryzs <jerrysizhang@gmail.com>
2021-10-01 14:49:49 -0400

    perf(Instruction): manage saved input with react

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,4 +1,4 @@
-import { KeyboardEvent, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 import { NextSeo } from 'next-seo'
 import styles from '../styles/pages/instruction.module.scss'
@@ -26,44 +26,55 @@ const Instruction = ({
   id: string
   instruction: string
 }): JSX.Element => {
+  const container = useRef<HTMLDivElement>(null)
+
+  const [content, setContent] = useState('')
+  const [edit, setEdit] = useState(false)
+
   const { data, mutate } = useSWR<API.InstructionGET>(
     `${$0.api.instruction}?id=${id}`,
     $0.fetch,
     {
       refreshInterval: 1000,
       fallbackData: { instruction },
+      isPaused: () => edit,
     }
   )
 
-  const container = useRef<HTMLDivElement>(null)
-
-  const [awaitSave, setAwaitSave] = useState(false)
-  const [edit, setEdit] = useState(false)
+  useEffect(() => {
+    if (!edit) {
+      setContent(data?.instruction ?? '')
+    }
+  }, [data])
 
   useEffect(() => {
     if (edit && container.current !== null) {
       window.getSelection()?.selectAllChildren(container.current)
     }
-  }, [edit])
 
-  useEffect(() => {
-    if (awaitSave) {
-      const text =
-        container.current?.textContent ?? container.current?.innerText ?? ''
-      fetch(`${$0.api.instruction}?id=${id}`, {
-        method: 'POST',
-        body: JSON.stringify({ instruction: text }),
-      })
+    if (!edit) {
       window.getSelection()?.removeAllRanges()
-      setAwaitSave(false)
-      setEdit(false)
       mutate()
     }
-  }, [awaitSave])
+  }, [edit])
 
   const handleEditClick = (e: MouseEvent<HTMLAnchorElement>): void => {
     e.preventDefault()
     setEdit(true)
+  }
+
+  let update: undefined | NodeJS.Timeout
+  const handleContentInput = (e: ChangeEvent<HTMLDivElement>): void => {
+    if (update === undefined) {
+      update = setTimeout(() => {
+        const text = (e.target.textContent ?? e.target.innerText ?? '').trim()
+        fetch(`${$0.api.instruction}?id=${id}`, {
+          method: 'POST',
+          body: JSON.stringify({ instruction: text }),
+        })
+        update = undefined
+      }, 2000)
+    }
   }
 
   const handleContentKeyDown = (e: KeyboardEvent<HTMLDivElement>): void => {
@@ -74,7 +85,7 @@ const Instruction = ({
   }
 
   const handleContentBlur = (): void => {
-    setAwaitSave(true)
+    setEdit(false)
   }
 
   return (
@@ -95,10 +106,12 @@ const Instruction = ({
           <div
             ref={container}
             contentEditable={edit}
+            suppressContentEditableWarning
+            onInput={handleContentInput}
             onBlur={handleContentBlur}
             onKeyDown={handleContentKeyDown}
           >
-            {data?.instruction ?? ''}
+            {content}
           </div>
         </div>
       </div>
@@ -106,4 +119,4 @@ const Instruction = ({
   )
 }
 
-export default Instruction
+export default InstructionPage
