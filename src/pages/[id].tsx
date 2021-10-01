b72c16a049b171262ba8848f2d6bf46f37b54307
Jerryzs <jerrysizhang@gmail.com>
2021-10-01 14:52:02 -0400

    feat(Components): Instruction Field

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,9 +1,9 @@
-import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import useSWR from 'swr'
 import { NextSeo } from 'next-seo'
+import InstructionField from '../components/InstructionField'
 import styles from '../styles/pages/instruction.module.scss'
 
-import type { MouseEvent } from 'react'
 import type { GetServerSideProps } from 'next'
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -19,19 +19,16 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 }
 
-const Instruction = ({
+const InstructionPage = ({
   id,
   instruction,
 }: {
   id: string
   instruction: string
 }): JSX.Element => {
-  const container = useRef<HTMLDivElement>(null)
-
-  const [content, setContent] = useState('')
   const [edit, setEdit] = useState(false)
 
-  const { data, mutate } = useSWR<API.InstructionGET>(
+  const insSWR = useSWR<API.InstructionGET>(
     `${$0.api.instruction}?id=${id}`,
     $0.fetch,
     {
@@ -41,78 +38,28 @@ const Instruction = ({
     }
   )
 
-  useEffect(() => {
-    if (!edit) {
-      setContent(data?.instruction ?? '')
-    }
-  }, [data])
-
-  useEffect(() => {
-    if (edit && container.current !== null) {
-      window.getSelection()?.selectAllChildren(container.current)
-    }
-
-    if (!edit) {
-      window.getSelection()?.removeAllRanges()
-      mutate()
-    }
-  }, [edit])
-
-  const handleEditClick = (e: MouseEvent<HTMLAnchorElement>): void => {
-    e.preventDefault()
-    setEdit(true)
-  }
-
-  let update: undefined | NodeJS.Timeout
-  const handleContentInput = (e: ChangeEvent<HTMLDivElement>): void => {
-    if (update === undefined) {
-      update = setTimeout(() => {
-        const text = (e.target.textContent ?? e.target.innerText ?? '').trim()
-        fetch(`${$0.api.instruction}?id=${id}`, {
-          method: 'POST',
-          body: JSON.stringify({ instruction: text }),
-        })
-        update = undefined
-      }, 2000)
-    }
-  }
-
-  const handleContentKeyDown = (e: KeyboardEvent<HTMLDivElement>): void => {
-    if (e.key === 'Enter') {
-      e.preventDefault()
-      container.current?.blur()
-    }
-  }
-
-  const handleContentBlur = (): void => {
-    setEdit(false)
-  }
-
   return (
     <>
-      <NextSeo title={id} />
+      <NextSeo title={id.toUpperCase()} />
 
       <div className={styles.wrapper}>
-        <div className={styles.instruction}>
-          <a
-            href='#'
-            style={{
-              color: edit ? '#ff0000' : '#000000',
-            }}
-            onClick={handleEditClick}
-          >
-            <i className='bi bi-pencil-square'></i>
+        <InstructionField
+          swr={insSWR}
+          onSave={(text) => {
+            fetch(`${$0.api.instruction}?id=${id}`, {
+              method: 'POST',
+              body: JSON.stringify({ instruction: text }),
+            })
+          }}
+          onStateChange={setEdit}
+        />
+
+        <div className={styles.share}>
+          Share your instruction with students using the link
+          <a href={`${$0.url}/${id}`}>
+            {`${$0.url}/`}
+            <b>{id.toUpperCase()}</b>
           </a>
-          <div
-            ref={container}
-            contentEditable={edit}
-            suppressContentEditableWarning
-            onInput={handleContentInput}
-            onBlur={handleContentBlur}
-            onKeyDown={handleContentKeyDown}
-          >
-            {content}
-          </div>
         </div>
       </div>
     </>
