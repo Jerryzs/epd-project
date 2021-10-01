998956a4c28e68479eb674bd0999cee5a29a4615
Jerryzs <jerrysizhang@gmail.com>
2021-09-30 21:11:04 -0400

    chore(App): setup next seo defaults

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/global.scss'
 
+import { DefaultSeo } from 'next-seo'
 import MainLayout from '../components/MainLayout'
 
 import type { AppProps } from 'next/app'
+import type { DefaultSeoProps } from 'next-seo'
+
+const SEO: DefaultSeoProps = {
+  titleTemplate: '%s - MyTask',
+}
 
 if (typeof window !== 'undefined') {
   window.bootstrap = require('bootstrap')
@@ -11,6 +17,7 @@ if (typeof window !== 'undefined') {
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
