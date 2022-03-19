c00b9f354e0b6ad8bf4db6646da91845ce45df23
Jerryzs <jerrysizhang@gmail.com>
2022-03-19 00:00:40 -0400

    refactor(app): move link and meta elements to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,94 @@ import 'typeface-fredoka-one'
 
 import { useEffect } from 'react'
 import useSWR from 'swr'
+import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
 
 import type { AppProps } from 'next/app'
 import type { DefaultSeoProps } from 'next-seo'
+import type { ReactFragment } from 'react'
 
 const SEO: DefaultSeoProps = {
   titleTemplate: '%s - MyTasks',
 }
+
+const MANIFEST_ICONS: ReactFragment = (
+  <>
+    <link
+      rel='apple-touch-icon'
+      sizes='57x57'
+      href='/icons/apple-touch-icon-57x57.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='60x60'
+      href='/icons/apple-touch-icon-60x60.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='72x72'
+      href='/icons/apple-touch-icon-72x72.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='76x76'
+      href='/icons/apple-touch-icon-76x76.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='114x114'
+      href='/icons/apple-touch-icon-114x114.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='120x120'
+      href='/icons/apple-touch-icon-120x120.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='144x144'
+      href='/icons/apple-touch-icon-144x144.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='152x152'
+      href='/icons/apple-touch-icon-152x152.png'
+    />
+    <link
+      rel='apple-touch-icon'
+      sizes='180x180'
+      href='/icons/apple-touch-icon-180x180.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='32x32'
+      href='/icons/favicon-32x32.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='194x194'
+      href='/icons/favicon-194x194.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='192x192'
+      href='/icons/android-chrome-192x192.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='16x16'
+      href='/icons/favicon-16x16.png'
+    />
+    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+    <link rel='manifest' href='/manifest.webmanifest' />
+    <meta name='msapplication-TileColor' content='#00aba9' />
+    <meta name='theme-color' content='#ffffff' />
+  </>
+)
 
 if (typeof window !== 'undefined') {
   window.bootstrap = require('bootstrap')
@@ -60,6 +140,13 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 
   return (
     <>
+      <Head>
+        {MANIFEST_ICONS}
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        />
+      </Head>
       <DefaultSeo {...SEO} />
       <Component user={data} {...pageProps} />
     </>
